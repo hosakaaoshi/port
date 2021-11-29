f456e20b6f73138b0b5709c0ada8959c4ac90c77
HosakaAoshi <aoshi97hosaka@gmail.com>
2021-11-29 08:06:44 +0000

    Initial Commit

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,7 +3,7 @@ class FavoritesController < ApplicationController
    @tweet = Tweet.find(params[:tweet_id])
    @favorite = Favorite.new(user_id: current_user.id, tweet_id: @tweet.id)
    @favorite.save
-   Notification.save_notification_comment!(current_user, @tweet.user_id, @tweet.id, @comment.id)
+   #Notification.save_notification_comment!(current_user, @tweet.user_id, @tweet.id)
     render 'favorites/create.js.erb'
   end
 
